a99359fe4318d1bc3da2d5c3d2e9dadb5fe1b4fb
Sergi <sergi.fernandez@basetis.com>
2023-10-12 18:16:45 +0200

    fix vercel

--- src/app/pages/devlog/devlog.component.ts
+++ src/app/pages/devlog/devlog.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-devlog',
   templateUrl: './devlog.component.html',
-  styleUrls: ['./devlog.component.scss']
 })
-export class DevlogComponent {
-
-}
+export class DevlogComponent {}
