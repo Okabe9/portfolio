dcb2e40b133932286d23c6934a63475c2a4db509
Sergi <sergi.fernandez@basetis.com>
2023-10-12 18:10:07 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'gamedev-portfolio-6d6c4',
+    appId: '1:627564484778:web:5160d1a2f6f678f2cad8d3',
+    storageBucket: 'gamedev-portfolio-6d6c4.appspot.com',
+    apiKey: 'AIzaSyDDXg6pnGHFyXYeL6P0KmbXIUT01EJbOXg',
+    authDomain: 'gamedev-portfolio-6d6c4.firebaseapp.com',
+    messagingSenderId: '627564484778',
+    measurementId: 'G-MB8TBYJMCB',
+  },
+};
